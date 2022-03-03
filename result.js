8d882e5de1c48086f3d5e21eabd2190191235e38
harsh joon <62814990+Harshjoon@users.noreply.github.com>
2022-03-03 06:10:05 +0530

    removed

--- result.js
+++
@@ -1,9 +0,0 @@
-console.log(responses)
-
-var totalPoints = 0
-for ( var i = 0; i < length; i++) {
-    totalPoints += responses[i].point
-}
-
-
-document.getElementById('result').innerHTML = totalPoints
